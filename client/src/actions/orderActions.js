89eb9fa192958b9215b960115e8b5141b2f5f68b
Anoop Jha <anoop.jha@siemens.com>
2024-01-10 01:07:51 +0530

    fixed error

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -1,69 +1,71 @@
-import instance from 'instance'
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://mama-john.onrender.com",
 });
 
 export const placeOrder = (token, subtotal) => async (dispatch, getState) => {
-  dispatch({ type: 'PLACE_ORDER_REQUEST' })
-  const currentUser = getState().loginUserReducer.currentUser
-  const cartItems = getState().cartReducer.cartItems
+  dispatch({ type: "PLACE_ORDER_REQUEST" });
+  const currentUser = getState().loginUserReducer.currentUser;
+  const cartItems = getState().cartReducer.cartItems;
 
   try {
-    const response = await instance.post('/api/orders/placeorder', {
+    const response = await instance.post("/api/orders/placeorder", {
       token,
       subtotal,
       currentUser,
       cartItems,
-    })
-    dispatch({ type: 'PLACE_ORDER_SUCCESS' })
-    console.log(response)
+    });
+    dispatch({ type: "PLACE_ORDER_SUCCESS" });
+    console.log(response);
   } catch (error) {
-    dispatch({ type: 'PLACE_ORDER_FAILED' })
-    console.log(error)
+    dispatch({ type: "PLACE_ORDER_FAILED" });
+    console.log(error);
   }
-}
+};
 
 export const getUserOrders = () => async (dispatch, getState) => {
-  const currentUser = getState().loginUserReducer.currentUser
-  dispatch({ type: 'GET_USER_ORDERS_REQUEST' })
+  const currentUser = getState().loginUserReducer.currentUser;
+  dispatch({ type: "GET_USER_ORDERS_REQUEST" });
 
   try {
-    const response = await instance.post('/api/orders/getuserorders', {
+    const response = await instance.post("/api/orders/getuserorders", {
       userid: currentUser._id,
-    })
+    });
 
-    console.log(response)
+    console.log(response);
 
-    dispatch({ type: 'GET_USER_ORDERS_SUCCESS', payload: response.data })
+    dispatch({ type: "GET_USER_ORDERS_SUCCESS", payload: response.data });
   } catch (error) {
-    dispatch({ type: 'GET_USER_ORDERS_FAILED', payload: error })
+    dispatch({ type: "GET_USER_ORDERS_FAILED", payload: error });
   }
-}
+};
 
 export const getAllOrders = () => async (dispatch, getState) => {
-  const currentUser = getState().loginUserReducer.currentUser
-  dispatch({ type: 'GET_ALLORDERS_REQUEST' })
+  const currentUser = getState().loginUserReducer.currentUser;
+  dispatch({ type: "GET_ALLORDERS_REQUEST" });
 
   try {
-    const response = await instance.get('/api/orders/getallorders')
+    const response = await instance.get("/api/orders/getallorders");
 
-    console.log(response)
+    console.log(response);
 
-    dispatch({ type: 'GET_ALLORDERS_SUCCESS', payload: response.data })
+    dispatch({ type: "GET_ALLORDERS_SUCCESS", payload: response.data });
   } catch (error) {
-    dispatch({ type: 'GET_ALLORDERS_FAILED', payload: error })
+    dispatch({ type: "GET_ALLORDERS_FAILED", payload: error });
   }
-}
+};
 
 export const deliverOrder = (orderid) => async (dispatch) => {
   try {
-    const response = await instance.post('/api/orders/deliverorder', { orderid })
-    console.log(response)
-    alert('Order Delivered')
-    const orders = await instance.get('/api/orders/getallorders')
-    dispatch({ type: 'GET_ALLORDERS_SUCCESS', payload: orders.data })
+    const response = await instance.post("/api/orders/deliverorder", {
+      orderid,
+    });
+    console.log(response);
+    alert("Order Delivered");
+    const orders = await instance.get("/api/orders/getallorders");
+    dispatch({ type: "GET_ALLORDERS_SUCCESS", payload: orders.data });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
